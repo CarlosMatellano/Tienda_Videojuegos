d55f699ad3064bd713883758eaabb6bad51e2653
CarlosMatellano <160237270+CarlosMatellano@users.noreply.github.com>
2024-04-24 10:49:21 +0200

    .

--- Game/src/main/java/com/ilerna/clientes/ClientesApplication.java
+++ Game/src/main/java/com/ilerna/clientes/ClientesApplication.java
@@ -1,13 +1,13 @@
-package com.mycompany.game;
+package com.ilerna.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Game {
+public class ClientesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Game.class, args);
+		SpringApplication.run(ClientesApplication.class, args);
 	}
 
 }
